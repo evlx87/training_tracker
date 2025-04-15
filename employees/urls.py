4529b364f855a2cedafe7701a884ba52dc6900ea
Sergey Semyonov <evilnyagaming@yandex.ru>
2025-04-15 13:24:18 +0300

    добавлены style.css и html шаблоны заглушки

--- employees/urls.py
+++ employees/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from employees.views import employee_list
+from employees.views import employee_list, index, departments, trainings, reports
 
 app_name = 'employees'
 
 urlpatterns = [
-    path('/', employee_list, name='employee_list'),
+    path('', index, name='index'),
+    path('employees/', employee_list, name='employee_list'),
+    path('departments/', departments, name='departments'),
+    path('trainings/', trainings, name='trainings'),
+    path('reports/', reports, name='reports'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -3,6 +3,22 @@ from .models import Employee
 
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
 def employee_list(request):
     employees = Employee.objects.all()
-    return render(request, 'employees/employee_list.html', {'employees': employees})
+    return render(request, 'employee_list.html', {'employees': employees})
+
+
+def departments(request):
+    return render(request, 'departments.html')
+
+
+def trainings(request):
+    return render(request, 'trainings.html')
+
+
+def reports(request):
+    return render(request, 'reports.html')
