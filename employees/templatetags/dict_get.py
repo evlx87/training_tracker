55b1d5117f5c2ebfaaf873b2d75c6fa68e109e4f
evlx <evlx@mail.ru>
2025-05-31 22:48:03 +0300

    обновлена логика вывода отчета в reports.html

--- employees/templatetags/dict_get.py
+++ employees/templatetags/dict_get.py
@@ -3,5 +3,22 @@ from django import template
 register = template.Library()
 
 @register.filter
-def get_item(dictionary, key):
+def dict_get(dictionary, key):
     return dictionary.get(key)
+
+@register.filter
+def int_key(dictionary, key):
+    try:
+        return dictionary.get(int(key))
+    except (ValueError, TypeError):
+        return None
+
+@register.filter
+def get_status_icon(status):
+    icons = {
+        'not-completed': '✖',
+        'overdue': '⏰',
+        'warning': '⚠',
+        'completed': '✔',
+    }
+    return icons.get(status, '✖')  # По умолчанию иконка для "не пройдено"
