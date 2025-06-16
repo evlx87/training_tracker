34c7475191c5bfc7c093926e0e2bdd3ad4fa538c
evlx <evlx@mail.ru>
2025-06-16 20:32:30 +0300

    в модель Position добавлена индексация

--- positions/models.py
+++ positions/models.py
@@ -5,11 +5,16 @@ class Position(models.Model):
     name = models.CharField(
         max_length=255,
         unique=True,
-        verbose_name='Название')
+        verbose_name='Название',
+        db_index=True)
     is_manager = models.BooleanField(
-        default=False, verbose_name='Руководитель')
+        default=False,
+        verbose_name='Руководитель',
+        db_index=True)
     is_teacher = models.BooleanField(
-        default=False, verbose_name='Педагогический работник')
+        default=False,
+        verbose_name='Педагогический работник',
+        db_index=True)
 
     def __str__(
             self):
